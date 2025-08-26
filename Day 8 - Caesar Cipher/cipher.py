1661f204ceb6b9ed0d30bdf4c1b525e37a42be24
arya-pradhan <aryashreepradhan@gmail.com>
2025-08-26 17:34:27 -0400

    creating the cipher

--- Day 8 - Caesar Cipher/cipher.py
+++ Day 8 - Caesar Cipher/cipher.py
@@ -3,3 +3,10 @@ alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
 direction = input("Type 'encode' to encrype, tyep 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for char in original_text:
+        new_index =  alphabet.index(char) + shift_amount
+        cipher_text += alphabet[new_index]
+    print(cipher_text)
