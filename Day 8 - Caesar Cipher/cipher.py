bcc61a07a700d1a0b4c7aa6d720473aab05f1d68
arya-pradhan <aryashreepradhan@gmail.com>
2025-08-26 20:11:04 -0400

    finishing the project

--- Day 8 - Caesar Cipher/cipher.py
+++ Day 8 - Caesar Cipher/cipher.py
@@ -1,28 +1,5 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-direction = input("Type 'encode' to encrype, tyep 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
-    for char in original_text:
-        new_index =  alphabet.index(char) + shift_amount
-
-        new_index %= len(alphabet)
-        cipher_text += alphabet[new_index]
-
-    print(f"Here is the encoded result: {cipher_text}")
-
-def decrypt(original_text, shift_amount):
-    decipher_text = ""
-    for char in original_text:
-        new_index = alphabet.index(char) - shift_amount
-
-        new_index %= len(alphabet)
-        decipher_text += alphabet[new_index]
-
-    print(f"Here is the decoded result: {decipher_text}")
 
 def caesar(original_text, shift_amount,choice):
     output_text = ""
@@ -30,12 +7,25 @@ def caesar(original_text, shift_amount,choice):
         shift_amount *= -1
 
     for char in original_text:
+        if char not in alphabet:
+            output_text += char
+        else:
+            new_index = alphabet.index(char) + shift_amount
 
-        new_index = alphabet.index(char) + shift_amount
+            new_index %= len(alphabet)
+            output_text += alphabet[new_index]
 
-        new_index %= len(alphabet)
-        output_text += alphabet[new_index]
+    print(f"Here is the {choice}d result: {output_text}")
 
-    print(f"Here is the {choice}ed result: {output_text}")
+redo = True
+while redo:
+    direction = input("Type 'encode' to encrype, tyep 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-caesar(original_text=text,shift_amount=shift,choice=direction)
+    caesar(original_text=text,shift_amount=shift,choice=direction)
+
+    choice = input("Do you want to do this again? Type 'y' if yes, or type 'n' for no: ").lower()
+
+    if choice == "n":
+        redo = False
