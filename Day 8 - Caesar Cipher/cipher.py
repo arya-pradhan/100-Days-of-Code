29c918b1748f8700f5512e1065247e25d86489c5
arya-pradhan <aryashreepradhan@gmail.com>
2025-08-26 20:00:27 -0400

    simplifed caesar

--- Day 8 - Caesar Cipher/cipher.py
+++ Day 8 - Caesar Cipher/cipher.py
@@ -13,3 +13,29 @@ def encrypt(original_text, shift_amount):
         cipher_text += alphabet[new_index]
 
     print(f"Here is the encoded result: {cipher_text}")
+
+def decrypt(original_text, shift_amount):
+    decipher_text = ""
+    for char in original_text:
+        new_index = alphabet.index(char) - shift_amount
+
+        new_index %= len(alphabet)
+        decipher_text += alphabet[new_index]
+
+    print(f"Here is the decoded result: {decipher_text}")
+
+def caesar(original_text, shift_amount,choice):
+    output_text = ""
+    if choice == "decode":
+        shift_amount *= -1
+
+    for char in original_text:
+
+        new_index = alphabet.index(char) + shift_amount
+
+        new_index %= len(alphabet)
+        output_text += alphabet[new_index]
+
+    print(f"Here is the {choice}ed result: {output_text}")
+
+caesar(original_text=text,shift_amount=shift,choice=direction)
